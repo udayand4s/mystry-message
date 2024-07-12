78c467b60711980825e1987c70264788567e2fe1
Coochiee <sugarytchannel@gmail.com>
2024-07-12 13:01:52 +0530

    main

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from 'next/navigation'
 import { signUpSchema } from '@/schemas/signUpSchema'
 import axios, {AxiosError} from 'axios';
+import { ApiResponse } from '@/types/ApiResponse'
 
 const page = () => {
   const [username, setUsername] = useState('')
@@ -40,14 +41,55 @@ const page = () => {
           const response=await axios.get('/api/check-username-unique?username=$(debouncedUsername')
           setUsernameMessage(response.data.message)
         } catch (error) {
-          
+          console.error("Error in signup", error)
+          const axiosError= error as AxiosError<ApiResponse>
+          setUsernameMessage(
+          axiosError.response?.data.message ?? "Error checking username"
+      )
+        }
+        finally {
+          setIsCheckingUsername(false)
         }
       }
     }
+    checkUsernameUnique()
   }, [debouncedUsername])
 
+  const onSubmit= async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try {
+     const response= await axios.post<ApiResponse>('/api/sign-up', data)
+     toast({
+      title: 'Success',
+      description: response.data.message
+     })
+     router.replace('/verify/${username}')
+     setIsSubmitting(false)
+    } catch (error) {
+      console.error("Error in signup", error)
+      const axiosError= error as AxiosError<ApiResponse>
+      let errorMessage= axiosError.response?.data.message
+      toast({
+        title: "signUpfailed",
+        description: errorMessage,
+        variant: "destructive"
+      })
+      setIsSubmitting(false)
+    }
+  }
+
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join True Feedback
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
